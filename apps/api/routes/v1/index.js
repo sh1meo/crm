ef59d91791d5d29526d2c463a72a142ea1fc2d0b
Roman Gaisin <r.gaisin@wazzup.online>
2024-03-02 01:11:12 +0300

    test

--- apps/api/routes/v1/index.js
+++ apps/api/routes/v1/index.js
@@ -4,6 +4,10 @@ const apiRouter = Router();
 
 apiRouter.get('/', async (req, res) => {
   res.status(200).json({ message: 'OK from api' });
-})
+});
+
+apiRouter.post('/', async (req, res) => {
+  res.status(200).json({ message: 'OK from api' })
+});
 
 export default apiRouter;
