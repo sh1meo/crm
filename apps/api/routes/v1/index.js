20f42b40717c990a6960f40b65e31a0d39e2abc2
nastya_twc <a.tivyakova@wazzup.online>
2024-03-05 18:11:33 +0700

    finish

--- apps/api/routes/v1/index.js
+++ apps/api/routes/v1/index.js
@@ -1,9 +1,20 @@
 import Router from 'express';
-
+import user from '../../../../libs/services/user';
 const apiRouter = Router();
 
 apiRouter.get('/', async (req, res) => {
 	res.status(200).json({ message: 'OK from api' });
+});
+
+apiRouter.post('/check_user_rights', async (req, res) => {
+	const body = req.body; // req = { body: { role: 'seller' } }
+	try {
+		const rights = user.checkUserRole(body.role);
+		res.status(200).json(rights);
+	}	catch (error) {
+		res.status(error.code).json(error);
+	}
+	
 });
 
 export default apiRouter;
--- libs/services/user.js
+++ libs/services/user.js
@@ -1,15 +1,19 @@
+import { errors } from '../constants/errors';
+
 class UserService {
-  constructor() {}
-  checkUserRole (userRole) {
-    if (userRole === 'admin') {
-        const userPermissions={
-            read: true,
-            write: true
-        };
-    } else if {
-        userRole === 'seller'
-    }
-}
+	constructor() {this.roles = ['admin', 'seller'];}
+	checkUserRole (userRole) {
+		if (!this.roles.includes(userRole)) {
+			throw errors.user.UNKNOWN_USER_ROLE; //{ code: 400, description: 'Role must be admin or seller', error: 'UNKNOWN_USER_ROLE' }
+		}
+		if (userRole === 'admin') {
+			return {
+				read: true,
+				write: true
+			};
+		}
+		return { read: true, write: false };
+	}
 }
 
 export default new UserService();
